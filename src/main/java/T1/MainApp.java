69d0d78a353a6dce237fecf7063503917e388e52
pds-pucrs <112732574+pds-pucrs@users.noreply.github.com>
2022-09-30 23:18:45 -0300

    Add files via upload

---
+++ src/main/java/T1/MainApp.java
@@ -0,0 +1,16 @@
+package T1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) throws IOException {
+        MainAppCalculos.somaImpares(4,5);
+        MainAppCalculos.somaImpares(13, 10);
+        MainAppCalculos.somaImpares(13, 10);
+        MainAppCalculos.somaImpares(6, 4);
+        MainAppCalculos.somaImpares(3, 3);
+        MainAppCalculos.somaImpares(3, 5);
+        MainAppCalculos.somaImpares(3, 8);
+    }
+}
